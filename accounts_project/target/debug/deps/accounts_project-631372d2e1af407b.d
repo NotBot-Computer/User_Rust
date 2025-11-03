0a3b527cd7a976cc63f3aff2f1e5124414d73f3c
NotBot-Computer <berkant.k2005@gmail.com>
2025-11-03 15:55:42 +0100

    Initial_Rust

---
+++ accounts_project/target/debug/deps/accounts_project-631372d2e1af407b.d
@@ -0,0 +1,5 @@
+/Users/berkantkucukomer/projects/accounts_project/target/debug/deps/accounts_project-631372d2e1af407b.d: src/main.rs
+
+/Users/berkantkucukomer/projects/accounts_project/target/debug/deps/libaccounts_project-631372d2e1af407b.rmeta: src/main.rs
+
+src/main.rs:
---
+++ accounts_project/target/debug/deps/accounts_project-a864494efbb7378d.d
@@ -0,0 +1,5 @@
+/Users/berkantkucukomer/projects/accounts_project/target/debug/deps/accounts_project-a864494efbb7378d.d: src/main.rs
+
+/Users/berkantkucukomer/projects/accounts_project/target/debug/deps/libaccounts_project-a864494efbb7378d.rmeta: src/main.rs
+
+src/main.rs:
